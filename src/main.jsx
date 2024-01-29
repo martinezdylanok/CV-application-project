e5b73a7313388dfa374fc02b1c85d89e9c3f639e
Lpik97 <dm97fx@gmail.com>
2024-01-29 10:56:33 +0100

    feat(main): add main component
    Introduce a new main component responsible for rendering the application's root structure. The component includes the PageTitle and GeneralInfo components within a React.StrictMode wrapper for enhanced development mode checks.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import PageTitle from "./components/pageTitle.jsx";
+import GeneralInfo from "./components/generalInfo.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+   <React.StrictMode>
+      <PageTitle />
+      <GeneralInfo />
+   </React.StrictMode>,
+);
