c8b62e1f2234b3e1514fd66f3532e1cb69161f3a
Lpik97 <dm97fx@gmail.com>
2024-01-31 10:17:41 +0100

    feat(main): add main component with PracticalExperience
    Introduce a new main component responsible for rendering the application's root structure. The component includes the PageTitle, GeneralInfo, EducationalInfo, and PracticalExperience components within a React.StrictMode wrapper for enhanced development mode checks.

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import "./index.css";
 import PageTitle from "./components/pageTitle.jsx";
 import GeneralInfo from "./components/generalInfo.jsx";
 import EducationalInfo from "./components/educationalInfo.jsx";
+import PracticalExperience from "./components/practicalExperience.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
    <React.StrictMode>
       <PageTitle />
       <GeneralInfo />
       <EducationalInfo />
+      <PracticalExperience />
    </React.StrictMode>,
 );
