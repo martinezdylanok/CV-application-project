e8f4108cbd116143e8b3ec1b1ca68f8bcb0b966e
Lpik97 <dm97fx@gmail.com>
2024-01-31 09:58:22 +0100

    feat(main): add main component with EducationalInfo
    Introduce a new main component responsible for rendering the application's root structure. The component includes the PageTitle, GeneralInfo, and EducationalInfo components within a React.StrictMode wrapper for enhanced development mode checks.

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import PageTitle from "./components/pageTitle.jsx";
 import GeneralInfo from "./components/generalInfo.jsx";
+import EducationalInfo from "./components/educationalInfo.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
    <React.StrictMode>
       <PageTitle />
       <GeneralInfo />
+      <EducationalInfo />
    </React.StrictMode>,
 );
