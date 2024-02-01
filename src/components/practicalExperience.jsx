866c5d633e628629dbb2b3176ff676589cbef889
Lpik97 <dm97fx@gmail.com>
2024-02-01 20:03:45 +0100

    feat(practical-info): update PracticalExperience component
    Refine the PracticalExperience component to improve the form structure, input placeholders, and overall user experience. The modifications include enhanced form inputs, updated placeholders, and improved styling for better usability. The associated practical-experience.css stylesheet has been updated accordingly to maintain a consistent and improved visual design.

--- src/components/practicalExperience.jsx
+++ src/components/practicalExperience.jsx
@@ -1,4 +1,5 @@
 import { React, useState } from "react";
+import "../styles/practical-experience.css";
 
 export default function PracticalExperience() {
    const [companyName, setCompanyName] = useState("");
@@ -18,43 +19,37 @@ export default function PracticalExperience() {
    }
 
    return (
-      <>
-         <h1>Practical Experience</h1>
+      <div className="practical-info-container">
+         <h1 className="practical-info-title">Practical Experience</h1>
          {!formSubmitted && (
-            <form onSubmit={handleSubmit}>
+            <form className="practical-info-form" onSubmit={handleSubmit}>
                <div className="company-name">
-                  <h1>Compnay Name</h1>
-                  <label htmlFor="company-name" />
-                  <input type="text" className="company-name" id="company-name" value={companyName} onChange={(e) => setCompanyName(e.target.value)} />
+                  <input type="text" className="company-name" id="company-name" placeholder="COMPANY NAME" maxLength={35} value={companyName} onChange={(e) => setCompanyName(e.target.value)} />
                </div>
                <div className="position-title">
-                  <h1>Position Title</h1>
-                  <label htmlFor="position-title" />
-                  <input type="text" className="position-title" id="position-title" value={positionTitle} onChange={(e) => setPositionTitle(e.target.value)} />
+                  <input type="text" className="position-title" id="position-title" placeholder="POSITION" maxLength={35} value={positionTitle} onChange={(e) => setPositionTitle(e.target.value)} />
                </div>
                <div className="main-responsabilities">
-                  <h1>Main Responsabilities</h1>
-                  <label htmlFor="main-responsabilities" />
-                  <input type="text" className="main-responsabilities" id="main-responsabilities" value={mainResponsabilities} onChange={(e) => setMainResponsabilities(e.target.value)} />
+                  <input type="text" className="main-responsabilities" id="main-responsabilities" placeholder="RESPONSABILITIES" maxLength={35} value={mainResponsabilities} onChange={(e) => setMainResponsabilities(e.target.value)} />
                </div>
                <div className="date-of-working">
-                  <h1>Date of working</h1>
-                  <label htmlFor="date-of-working" />
-                  <input type="text" className="date-of-working" id="date-of-working" value={dateOfWorking} onChange={(e) => setDateOfWorking(e.target.value)} />
+                  <input type="date" className="date-of-working" id="date-of-working" placeholder="DATE" maxLength={35} value={dateOfWorking} onChange={(e) => setDateOfWorking(e.target.value)} />
                </div>
-               <button type="submit">Submit</button>
+               <button type="submit">SUBMIT</button>
             </form>
          )}
 
          {formSubmitted && (
-            <form className="displayed-info" onSubmit={handleEdit}>
-               <h2>{companyName}</h2>
-               <p>Position title: {positionTitle}</p>
-               <p>Main responsabilities: {mainResponsabilities}</p>
-               <p>Date of working: {dateOfWorking}</p>
-               <button type="submit">Edit</button>
+            <form className="displayed-info-container" onSubmit={handleEdit}>
+               <div className="displayed-data">
+                  <h2>{companyName}</h2>
+                  <p>Position title: {positionTitle}</p>
+                  <p>Main responsabilities: {mainResponsabilities}</p>
+                  <p>Date of working: {dateOfWorking}</p>
+               </div>
+               <button type="submit">EDIT</button>
             </form>
          )}
-      </>
+      </div>
    );
 }
